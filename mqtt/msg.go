6272fa2aecb6f0181eef0bd7a3be339bf61fa759
well <347471159@qq.com>
2024-08-20 08:34:28 +0800

    update

--- mqtt/msg.go
+++ mqtt/msg.go
@@ -7,17 +7,11 @@ import (
 )
 
 type Action string
-type Page string
 
 const (
 	ActionRefresh Action = "refresh" // 刷新
 	ActionRefund  Action = "refund"  // 请求退款
 	ActionDelete  Action = "delete"  // 删除
-
-	PageRequirements      Page = "requirements"
-	PageRequirementDetail Page = "requirement_detail"
-	PageWaybills          Page = "waybills"
-	PageWaybillDetail     Page = "waybill_detail"
 )
 
 type Message struct {
@@ -28,33 +22,22 @@ type Message struct {
 	WaybillId     uint64
 }
 
-func PublishMqttTopics(mqttClient mqtt.Client, param Message, pages ...Page) {
+func PublishMqttTopics(mqttClient mqtt.Client, param Message) {
 
-	if len(pages) > 0 {
+	if param.MerchantId != 0 {
+		topic := fmt.Sprintf("yls/%d/%d/%d/%s", param.MerchantId, param.RequirementId, param.WaybillId, param.Action)
 
-		for _, page := range pages {
-			if param.MerchantId != 0 {
-				topic := fmt.Sprintf("yls/%d/%s/%d/%d/%s", param.MerchantId, page, param.RequirementId, param.WaybillId, param.Action)
+		mqttClient.Publish(topic, 0, false, "")
 
-				mqttClient.Publish(topic, 0, false, "")
+		log.Printf("pub [%s] %s\n", topic, "")
+	}
 
-				log.Printf("pub [%s] %s\n", topic, "")
-			}
+	if param.DriverId != 0 {
+		topic := fmt.Sprintf("yls/%d/%d/%d/%s", param.DriverId, param.RequirementId, param.WaybillId, param.Action)
 
-		}
+		mqttClient.Publish(topic, 0, false, "")
 
-		for _, page := range pages {
-
-			if param.DriverId != 0 {
-				topic := fmt.Sprintf("yls/%d/%s/%d/%d/%s", param.DriverId, page, param.RequirementId, param.WaybillId, param.Action)
-
-				mqttClient.Publish(topic, 0, false, "")
-
-				log.Printf("pub [%s] %s\n", topic, "")
-			}
-
-		}
-
+		log.Printf("pub [%s] %s\n", topic, "")
 	}
 
 	return
